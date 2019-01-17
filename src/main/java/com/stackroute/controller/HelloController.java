625989704c56b07aab2da00d732416ae2056fd79
amit-s-k <amitsk2014@gmail.com>
2019-01-17 22:57:49 +0530

    refactored solution For excercise 2

--- src/main/java/com/stackroute/controller/HelloController.java
+++ src/main/java/com/stackroute/controller/HelloController.java
@@ -3,30 +3,27 @@ package com.stackroute.controller;
 import com.stackroute.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
     @RequestMapping("/hey")
-    public String sayHello(Model model){
-        model.addAttribute("msg","Hello Earth");
+    public String sayHello(Model model) {
+        model.addAttribute("msg", "Hello Earth");
         return "index";
     }
 
     @RequestMapping("login")
-    public String showLoginPage(){
+    public String showLoginPage() {
         return "login";
     }
+
     @RequestMapping("showMessage")
-    public ModelAndView showMessagePage(@RequestParam("firstName") String userName,@RequestParam("password") String password){
-        ModelAndView modelAndView =new ModelAndView();
-        User user=new User();
-        user.setUserName(userName);
-        user.setPassword(password);
+    public ModelAndView showMessage(@ModelAttribute("user") User user, ModelAndView modelAndView) {
         modelAndView.setViewName("message");
-        modelAndView.addObject("userObject",user);
+        modelAndView.addObject("userObject", user);
         return modelAndView;
     }
 }
--- src/main/java/com/stackroute/model/User.java
+++ src/main/java/com/stackroute/model/User.java
@@ -4,22 +4,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class User {
-    private String userName;
+    private String firstName;
     private String password;
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
